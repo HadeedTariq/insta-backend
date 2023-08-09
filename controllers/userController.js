685d9d4999c382fb1d8bd9a57747607887789463
HadeedTariq <computeranalog351@gmail.com>
2023-08-09 15:07:08 +0500

    make some changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,6 +30,7 @@ const authenticateUser = async (req, res) => {
         const isPasswordVerified = await bcrypt.compare(password, user.password)
         if (!isPasswordVerified) return res.status(404).json({ message: "Password Incorrect" })
         const token = jwt.sign({ name: user.name, id: user._id, userImage: user.userImage,description:user.description,hobby:user.hobby }, process.env.JWT_SECRET)
+    res.set('Access-Control-Allow-Origin', '*')
         return res.cookie('instaUser', token,{sameSite:"none"}).status(200).json({ message: "User loged in successfully" })
     } catch (error) {
         return res.status(404).json({ message: "Something went wrong" })
@@ -42,6 +43,7 @@ const deleteUser = async (req, res) => {
         const user = jwt.verify(instaUser, process.env.JWT_SECRET)
         if (!user) return res.status(404).json({ message: "Invalid Token" })
         await User.deleteOne({ name: user.name })
+    res.set('Access-Control-Allow-Origin', '*')
         return res.cookie('instaUser', '',{sameSite:"none"}).status(200).json({ message: "User deleted  successfully" })
     } catch (err) {
         console.log(err);
@@ -59,6 +61,7 @@ const updateUserPassword = async (req, res) => {
         if (!isPasswordVerified) return res.status(404).json({ message: "Password Incorrect" })
         const newHashedPassword = await bcrypt.hash(newPassword, 10)
         await User.findByIdAndUpdate({ _id: user._id }, { password: newHashedPassword })
+        res.set('Access-Control-Allow-Origin', '*')
         return res.cookie('instaUser', '',{sameSite:"none"}).status(200).json({ message: "Password Updated Successfully" })
     } catch (error) {
         console.log(error);
@@ -82,6 +85,7 @@ const updateUserDetails = async (req, res) => {
                 return res.status(404).json({ message: "Something went wrong" })
             } else {
                 await User.findByIdAndUpdate({ _id: user._id }, { description: description, hobby: hobby,userImage:fileName })
+                res.set('Access-Control-Allow-Origin', '*')
                 return res.cookie('instaUser','').status(200).json({ message: "Saved Successfully" })
             }
         })
@@ -112,6 +116,7 @@ const getAllUser = async (req, res) => {
     return res.status(200).json(users)
 }
 const logOutUser = async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*')
     return res.cookie('instaUser', '',{sameSite:"none"}).status(200).json({ message: "User loged out successfully" })
 }
 
