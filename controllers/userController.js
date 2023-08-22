20b6b37f0347474fb317145cce303fcef95d3f65
HadeedTariq <computeranalog351@gmail.com>
2023-08-22 11:03:37 +0500

    final push

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,141 +1,180 @@
-import { User } from '../models/User.js'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+import { User } from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const createUser = async (req, res) => {
-    const { name, email, password, userImage } = req.body
-    if (!name || !email || !password) {
-        return res.status(404).json({ message: "Please fill all the fields" })
-    }
-    const user = await User.findOne({ name })
-    if (user) {
-        return res.status(404).json({ message: "User already existed" })
-    }
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const newUser = await User.create({
-            name, email, password: hashedPassword
-        })
-        return res.status(200).json(newUser)
-    } catch (error) {
-        return res.status(404).json({ message: "Something went wrong" })
-    }
+  const { name, email, password, userImage } = req.body;
+  if (!name || !email || !password) {
+    return res.status(404).json({ message: "Please fill all the fields" });
+  }
+  const user = await User.findOne({ name });
+  if (user) {
+    return res.status(404).json({ message: "User already existed" });
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return res.status(200).json(newUser);
+  } catch (error) {
+    return res.status(404).json({ message: "Something went wrong" });
+  }
 };
 const authenticateUser = async (req, res) => {
-    const { name, password } = req.body
-    if (!name || !password) return res.status(404).json({ message: "Please fill all the fields" })
-    try {
-        const user = await User.findOne({ name })
-        if (!user) return res.status(404).json({ message: "User not found" })
-        const isPasswordVerified = await bcrypt.compare(password, user.password)
-        if (!isPasswordVerified) return res.status(404).json({ message: "Password Incorrect" })
-        const token = jwt.sign({ name: user.name, id: user._id, userImage: user.userImage, description: user.description, hobby: user.hobby }, process.env.JWT_SECRET)
-        return res.cookie('instaUser', token, {
-            httpOnly: true,
-            sameSite: 'lax',
-            maxAge: 30 * 24 * 60 * 60 * 1000,
-        }).status(200).json({ message: "User loged in successfully" })
-          
-    } catch (error) {
-        return res.status(404).json({ message: "Something went wrong" })
-    }
+  const { name, password } = req.body;
+  if (!name || !password)
+    return res.status(404).json({ message: "Please fill all the fields" });
+  try {
+    const user = await User.findOne({ name });
+    if (!user) return res.status(404).json({ message: "User not found" });
+    const isPasswordVerified = await bcrypt.compare(password, user.password);
+    if (!isPasswordVerified)
+      return res.status(404).json({ message: "Password Incorrect" });
+    const token = jwt.sign(
+      {
+        name: user.name,
+        id: user._id,
+        userImage: user.userImage,
+        description: user.description,
+        hobby: user.hobby,
+      },
+      process.env.JWT_SECRET
+    );
+    return res
+      .cookie("instaUser", token, {
+        httpOnly: true,
+        sameSite: "lax",
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({ message: "User loged in successfully" });
+  } catch (error) {
+    return res.status(404).json({ message: "Something went wrong" });
+  }
 };
 const deleteUser = async (req, res) => {
-    const { instaUser } = req.body
-    try {
-        if (!instaUser) return res.status(404).json({ message: "Not token found" })
-        const user = jwt.verify(instaUser, process.env.JWT_SECRET)
-        if (!user) return res.status(404).json({ message: "Invalid Token" })
-        await User.deleteOne({ name: user.name })
-        return res.cookie('instaUser', '', {
-            httpOnly: true,
-            sameSite: 'lax',
-            maxAge: 30 * 24 * 60 * 60 * 1000,
-        }).status(200).json({ message: "User deleted  successfully" })
-    } catch (err) {
-        console.log(err);
-        return res.status(404).json({ message: "Something went wrong" })
-    }
+  const { instaUser } = req.body;
+  try {
+    if (!instaUser) return res.status(404).json({ message: "Not token found" });
+    const user = jwt.verify(instaUser, process.env.JWT_SECRET);
+    if (!user) return res.status(404).json({ message: "Invalid Token" });
+    await User.deleteOne({ name: user.name });
+    return res
+      .cookie("instaUser", "", {
+        httpOnly: true,
+        sameSite: "lax",
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({ message: "User deleted  successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ message: "Something went wrong" });
+  }
 };
 
 const updateUserPassword = async (req, res) => {
-    const { name, oldPassword, newPassword } = req.body;
-    if (!name || !oldPassword || !newPassword) return res.status(404).json({ message: "Please fill all the fields" })
-    try {
-        const user = await User.findOne({ name })
-        if (!user) return res.status(404).json({ message: "User not found" })
-        const isPasswordVerified = await bcrypt.compare(oldPassword, user.password)
-        if (!isPasswordVerified) return res.status(404).json({ message: "Password Incorrect" })
-        const newHashedPassword = await bcrypt.hash(newPassword, 10)
-        await User.findByIdAndUpdate({ _id: user._id }, { password: newHashedPassword })
-        response.set('A-Control-Allow-Origin', 'https://insta-frontend-six.vercel.app')
-        return res.cookie('instaUser', '', {
-            httpOnly: true,
-            sameSite: 'lax',
-            maxAge: 30 * 24 * 60 * 60 * 1000,
-        }).status(200).json({ message: "Password Updated Successfully" })
-    } catch (error) {
-        console.log(error);
-        return res.status(404).json({ message: "Something went wrong" })
-    }
-}
+  const { name, oldPassword, newPassword } = req.body;
+  if (!name || !oldPassword || !newPassword)
+    return res.status(404).json({ message: "Please fill all the fields" });
+  try {
+    const user = await User.findOne({ name });
+    if (!user) return res.status(404).json({ message: "User not found" });
+    const isPasswordVerified = await bcrypt.compare(oldPassword, user.password);
+    if (!isPasswordVerified)
+      return res.status(404).json({ message: "Password Incorrect" });
+    const newHashedPassword = await bcrypt.hash(newPassword, 10);
+    await User.findByIdAndUpdate(
+      { _id: user._id },
+      { password: newHashedPassword }
+    );
+    response.set(
+      "A-Control-Allow-Origin",
+      "https://insta-frontend-six.vercel.app"
+    );
+    return res
+      .cookie("instaUser", "", {
+        httpOnly: true,
+        sameSite: "lax",
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({ message: "Password Updated Successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({ message: "Something went wrong" });
+  }
+};
 const updateUserDetails = async (req, res) => {
-    const { name, description, hobby } = req.body;
-    const fileObject = req.files
-    if (!name || !hobby || !description || !fileObject) return res.status(404).json({ message: "Please fill all the fields" })
-    const fileKey = Object.keys(fileObject)[0];
-    const file = fileObject[fileKey];
-    let fileName = fileObject[fileKey].name;
-    fileName = Date.now() + fileName
-    try {
-        const user = await User.findOne({ name })
-        if (!user) return res.status(404).json({ message: "User not found" })
-        file.mv('./public/users/' + fileName, async function (err) {
-            if (err) {
-                console.log(err);
-                return res.status(404).json({ message: "Something went wrong" })
-            } else {
-                await User.findByIdAndUpdate({ _id: user._id }, { description: description, hobby: hobby, userImage: fileName })
-                response.set('A-Control-Allow-Origin', 'https://insta-frontend-six.vercel.app')
-                return res.cookie('instaUser', '', {
-                    httpOnly: true,
-                    sameSite: 'lax',
-                    maxAge: 30 * 24 * 60 * 60 * 1000,
-                }).status(200).json({ message: "Saved Successfully" })
-            }
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(404).json({ message: "Something went wrong" })
-    }
-}
+  const { name, description, hobby, userImage } = req.body;
+  if (!name || !hobby || !description || !userImage)
+    return res.status(404).json({ message: "Please fill all the fields" });
+  try {
+    const user = await User.findOne({ name });
+    if (!user) return res.status(404).json({ message: "User not found" });
+    await User.findByIdAndUpdate(
+      { _id: user._id },
+      { description: description, hobby: hobby, userImage: userImage }
+    );
+    return res
+      .cookie("instaUser", "", {
+        httpOnly: true,
+        sameSite: "lax",
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({ message: "Saved Successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({ message: "Something went wrong" });
+  }
+};
 
 const getSingleUser = async (req, res) => {
-    const { instaUser } = req.cookies
-    try {
-        if (!instaUser) return res.status(404).json({ message: "Not token found" })
-        const user = jwt.verify(instaUser, process.env.JWT_SECRET)
-        if (!user) return res.status(404).json({ message: "Invalid Token" })
-        return res.status(200).json(user)
-    } catch (err) {
-        console.log(err);
-        return res.status(404).json({ message: "Something went wrong" })
-    }
-}
+  const { instaUser } = req.cookies;
+  try {
+    if (!instaUser) return res.status(404).json({ message: "Not token found" });
+    const user = jwt.verify(instaUser, process.env.JWT_SECRET);
+    if (!user) return res.status(404).json({ message: "Invalid Token" });
+    return res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ message: "Something went wrong" });
+  }
+};
 const getAllUser = async (req, res) => {
-    const user = await User.find({})
-    const users = user.map(single => {
-        return { name: single.name, userImage: single.userImage, description: single.description, hobby: single.hobby }
-    }
-    )
-    return res.status(200).json(users)
-}
+  const user = await User.find({});
+  const users = user.map((single) => {
+    return {
+      name: single.name,
+      userImage: single.userImage,
+      description: single.description,
+      hobby: single.hobby,
+    };
+  });
+  return res.status(200).json(users);
+};
 const logOutUser = async (req, res) => {
-    return res.cookie('instaUser', '', {
-        httpOnly: true,
-        sameSite: 'lax',
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-    }).status(200).json({ message: "User loged out successfully" })
-}
+  return res
+    .cookie("instaUser", "", {
+      httpOnly: true,
+      sameSite: "lax",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    })
+    .status(200)
+    .json({ message: "User loged out successfully" });
+};
 
-export { createUser, deleteUser, authenticateUser, getAllUser, updateUserPassword, getSingleUser, logOutUser, updateUserDetails }
+export {
+  createUser,
+  deleteUser,
+  authenticateUser,
+  getAllUser,
+  updateUserPassword,
+  getSingleUser,
+  logOutUser,
+  updateUserDetails,
+};
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,36 +1,29 @@
 import { Router } from "express";
-import upload from 'express-fileupload'
+import upload from "express-fileupload";
 import { Post } from "../models/Post.js";
-const router = Router()
-router.use(upload())
-router.post('/create', async (req, res) => {
-    const { userImage, userName, description, tags } = req.body;
-    const fileObject=req.files
-    if (!userImage || !userName || !description || !tags || !fileObject){
-        return res.status(404).json({message:"Please fill all the fields"})
-    }
-    const fileKey = Object.keys(fileObject)[0];
-    const file = fileObject[fileKey];
-    let fileName = fileObject[fileKey].name;
-    let postImage =  Date.now()+fileName
-    try {
-      file.mv('/public/posts/' + postImage, async function (err) {
-        // /React Projects/mern-project/insta-clone/client/public/posts/
-        if (err) {
-            console.log(err);
-            return res.status(404).json({ message: "Something went wrong" })
-        } else {
-            const createPost=await Post.create({userImage, userName, description, postImage,tags})
-            return res.status(200).json({message:"Post created successfully"})   
-        }
-    })
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({message:"Something went wrong"})
-    }
-})
-router.get('/',async(req,res)=>{
-    const allPosts=await Post.find({})
-    return res.status(200).json(allPosts)
-})
-export { router as postRouter }
+const router = Router();
+router.use(upload());
+router.post("/create", async (req, res) => {
+  const { userImage, userName, description, tags, postImage } = req.body;
+  if (!userImage || !userName || !description || !tags || !postImage) {
+    return res.status(404).json({ message: "Please fill all the fields" });
+  }
+  try {
+    const createPost = await Post.create({
+      userImage,
+      userName,
+      description,
+      postImage,
+      tags,
+    });
+    return res.status(200).json({ message: "Post created successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: "Something went wrong" });
+  }
+});
+router.get("/", async (req, res) => {
+  const allPosts = await Post.find({});
+  return res.status(200).json(allPosts);
+});
+export { router as postRouter };
